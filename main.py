3e52752384cc03ce1fd15b9ab683d8d740f1b9b0
Corbin McDougal <mcdoucor000@warren.k12.in.us>
2024-10-02 08:43:59 -0400

    Initial commit

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ pygame.init()
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Cookie Monster Isn't A Muppet")
-pygame.display.set_icon(pygame.image.load('CookieMonster2.png'))
+pygame.display.set_caption("Catch The Clown")
 
 # Set FPS and clock
 FPS = 60
@@ -17,79 +16,74 @@ clock = pygame.time.Clock()
 
 # Set game values, Constants
 Starting_Lives = 5
-Starting_Velocity = 10
-Starting_Coin_Velocity = 10
-Starting_Coin_Acceleration = 0.5
+Clown_Velocity = 10
+Starting_Clown_Velocity = 10
+Clown_Acceleration = 0.5
 score = 0
 Buffer_Distance = 100
-Speed = 1
+clown_dx = random.choice(-1, 1)
+clown_dy = random.choice(-1, 1)
 
 '''constant player lives set it = to the constant above'''
 Lives = Starting_Lives
 '''constant coin velocity  set it = to the constant above'''
-CoinVelocity = Starting_Coin_Velocity
-PlayerSpeed = Starting_Velocity
+CoinVelocity = Starting_Clown_Velocity
+PlayerSpeed = Clown_Velocity
 
 # Set colors
-OneEyedONneHornedFlyingPurplePeopleEater = (15, 0, 20)
-ORANGE = (255, 159, 0)
-RED = (86, 3, 25)
-GREEN = (0, 255, 0)
-DARK_GREEN = (10, 50, 10)
-WHITE = (255, 255, 255)
+BLUE = (1, 175, 209)
+YELLOW = (248, 231, 28)
 BLACK = (0, 0, 0)
 
 # Set fonts
-font = pygame.font.Font('AttackGraffiti.ttf', 32)
-fontgo = pygame.font.Font('EBGaramond-VariableFont_wght.ttf', 40)
+font = pygame.font.Font('Assets/Franxurter.ttf', 32)
 
 
 # Set text
 # Score Text
-score_text = font.render("Score:" + str(score), True, GREEN, DARK_GREEN)
+score_text = font.render("Score:" + str(score), True, BLUE, YELLOW)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10, 10)
 
 # Title Text
 ''' same deal as score'''
-title_text = font.render("Feed The Dragon", True, RED, OneEyedONneHornedFlyingPurplePeopleEater)
+title_text = font.render("Feed The Dragon", True, BLUE, YELLOW)
 title_rect = title_text.get_rect()
 title_rect.centerx = WINDOW_WIDTH / 2
 title_rect.y = 10
 
 # Lives Text
-lives_text = font.render("Lives:" + str(Lives), True, GREEN, DARK_GREEN)
+lives_text = font.render("Lives:" + str(Lives), True, BLUE, YELLOW)
 Lives_rect = lives_text.get_rect()
 Lives_rect.topright = (WINDOW_WIDTH - 10, 10)
 
 # You Suck Text
-game_over_text = fontgo.render("You Suck. Do Better.", True, RED, OneEyedONneHornedFlyingPurplePeopleEater)
+game_over_text = font.render("You Suck. Do Better.", True, BLUE, YELLOW)
 game_over_rect = game_over_text.get_rect()
 game_over_rect.center = (WINDOW_WIDTH // 2, (WINDOW_HEIGHT // 2) - 10)
 
 # Continue Text
-continue_text = fontgo.render("Would You Like To Try Again?", True, RED, OneEyedONneHornedFlyingPurplePeopleEater)
+continue_text = font.render("Would You Like To Try Again?", True, BLUE, YELLOW)
 continue_text_rect = continue_text.get_rect()
 continue_text_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 32)
 
 #Set sound & music
-coin_sound = pygame.mixer.Sound("coin_sound.wav")
+click_sound = pygame.mixer.Sound("Assets/click_sound.wav")
 
-miss_sound = pygame.mixer.Sound("miss_sound.wav")
-#miss_sound_volume = pygame.mixer_music.set_volume(0.1)
-pygame.mixer.music.load("ftd_background_music.wav")
+miss_sound = pygame.mixer.Sound("Assets/miss_sound.wav")
+
+pygame.mixer.music.load("Assets/ctc_background_music.wav")
 
 
 #Images
-player_image = pygame.image.load("dragon_right.png")
-player_rect = player_image.get_rect()
-player_rect.left = 32
-player_rect.centery = WINDOW_HEIGHT // 2
+Clown_image = pygame.image.load("Assets/clown.png")
+Clown_rect = Clown_image.get_rect()
+Clown_rect.center = (WINDOW_HEIGHT // 2, WINDOW_WIDTH // 2)
 
-coin_image = pygame.image.load("coin.png")
-coin_rect = coin_image.get_rect()
-coin_rect.centerx = WINDOW_WIDTH + Buffer_Distance
-coin_rect.centery = random.randint(64, WINDOW_HEIGHT - 32)
+
+background_image = pygame.image.load("Assets/background.png")
+background_rect = background_image.get_rect()
+background_rect.topleft = (0, 0)
 
 pygame.mixer.music.play(-1, 0.0)
 
@@ -101,38 +95,29 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #Check for movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and player_rect.top > 64:
-        player_rect.y -= Starting_Velocity
-    if keys[pygame.K_DOWN] and player_rect.bottom < WINDOW_HEIGHT:
-        player_rect.y += Starting_Velocity
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x = event.pos[0]
+            mouse_y = event.pos[1]
 
-    #Coin Movement
-    if coin_rect.x < 0:
-        #Miss Coin
-        Lives -= 1
-        miss_sound.play()
-        coin_rect.centerx = WINDOW_WIDTH + Buffer_Distance
-        coin_rect.centery = random.randint(64, WINDOW_HEIGHT - 32)
-    else:
-        #Move the Coin
-        coin_rect.x -= CoinVelocity
+        if Clown_rect.collidepoint(mouse_x, mouse_y):
+            click_sound.play()
+            score += 1
+            Clown_Velocity += Clown_Acceleration
 
-    #Check for Collisions
-    if player_rect.colliderect(coin_rect):
-        score += 1
-        coin_sound.play()
-        CoinVelocity += Starting_Coin_Acceleration
-        PlayerSpeed += Speed
-        coin_rect.centerx = WINDOW_WIDTH + Buffer_Distance
-        coin_rect.centery = random.randint(64, WINDOW_HEIGHT - 32)
+            previous_dx = clown_dx
+            previous_dy = clown_dy
+
+
+
+
+
+
 
 
 
     # UPDATE HUD
-    score_text = font.render("Score:" + str(score), True, GREEN, DARK_GREEN)
-    lives_text = font.render("Lives:" + str(Lives), True, GREEN, DARK_GREEN)
+    score_text = font.render("Score:" + str(score), True, YELLOW)
+    lives_text = font.render("Lives:" + str(Lives), True, YELLOW)
 
     #Game Over Check
     if Lives == 0:
@@ -146,28 +131,20 @@ while running:
         while is_paused:
             for event in pygame.event.get():
                 #play again
-                if event.type == pygame.KEYDOWN:
-                    score = 0
-                    Lives = Starting_Lives
-                    player_rect.y = WINDOW_HEIGHT // 2
-                    CoinVelocity = Starting_Coin_Velocity
-                    PlayerSpeed = Starting_Velocity
-                    pygame.mixer.music.play(-1, 0.0)
-                    is_paused = False
+
                 if event.type == pygame.QUIT:
                     is_paused = False
                     running = False
 
 
     # Fill the Display
-    display_surface.fill(OneEyedONneHornedFlyingPurplePeopleEater)
+    display_surface.blit(background_image, background_rect)
 
     display_surface.blit(score_text, score_rect)
     display_surface.blit(title_text, title_rect)
     display_surface.blit(lives_text, Lives_rect)
-    display_surface.blit(player_image, player_rect)
-    display_surface.blit(coin_image, coin_rect)
-    pygame.draw.line(display_surface, ORANGE, (0, 64), (WINDOW_WIDTH, 64), 2)
+    display_surface.blit(Clown_image, Clown_rect)
+    pygame.draw.line(display_surface, BLACK, (0, 64), (WINDOW_WIDTH, 64), 2)
 
 
 
